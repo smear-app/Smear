8da96c0aef8f307337af59e8d9abbdae6b8afb65
Jaden Phan <48921711+jphan10@users.noreply.github.com>
2026-03-23 15:07:18 -0700

    feat(): added dependencies for ios local deploy + instructions (#29)

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.smearapp.smear',
+  appName: 'Smear',
+  webDir: 'dist'
+};
+
+export default config;
